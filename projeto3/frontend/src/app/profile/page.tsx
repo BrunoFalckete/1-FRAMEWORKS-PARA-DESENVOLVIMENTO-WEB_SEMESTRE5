197057ca2c2e7a6cec881111bf6081171d5d2b96
Bruno Falckete <bruno.fonseca@estudante.ifms.edu.br>
2023-11-16 21:05:18 -0400

    16-11-23

--- projeto3/frontend/src/app/profile/page.tsx
+++ projeto3/frontend/src/app/profile/page.tsx
@@ -1,9 +1,18 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { Tabs, TabsProps } from 'antd';
 import CategoriesList from './components/CategoriesList';
+import router from 'next/router';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 function Profile() {
+
+  const searchParams = useSearchParams()
+  const id = searchParams.get("id")
+
+  const [selectedTab, setSelectedTab] = useState<string>(id || "1");
+
+  const router = useRouter();
 
   const items: TabsProps['items'] = [
     {
@@ -30,9 +39,15 @@ function Profile() {
 
   return (
     <div className='p-5'>
-      <Tabs defaultActiveKey="1"items={items}>
-
-      </Tabs>
+      <Tabs
+      defaultActiveKey="1"
+      items={items}
+      onChange={(key) => {
+        router.push(`/profile?id=${key}`);
+        setSelectedTab(key);
+      }}
+      activeKey={selectedTab}
+      ></Tabs>
     </div>
   );
 }
